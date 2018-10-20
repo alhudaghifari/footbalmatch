af5bd07d09ec3091f120f057cab9d929c55f0263
alhudaghifari <alhudaghifari@gmail.com>
2018-10-20 21:29:12 +0700

    list team interface and presenter added

--- app/src/main/java/com/example/ghifar/footballmatch/presenter/teams/ListTeamsInterface.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/presenter/teams/ListTeamsInterface.kt
@@ -1,9 +1,15 @@
 package com.example.ghifar.footballmatch.presenter.teams
 
+import com.example.ghifar.footballmatch.model.datateammodel.Team
+
 
 /**
  * Created by Alhudaghifari on 20:26 20/10/18
  *
  */
 interface ListTeamsInterface {
+    fun showLoading()
+    fun hideLoading()
+    fun showTeamList(data: List<Team>)
+    fun showError(msg: String)
 }
--- app/src/main/java/com/example/ghifar/footballmatch/presenter/teams/ListTeamsPresenter.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/presenter/teams/ListTeamsPresenter.kt
@@ -1,7 +1,41 @@
 package com.example.ghifar.footballmatch.presenter.teams
 
+import com.example.ghifar.footballmatch.model.eventleaguemodel.EventLeagueModel
+import com.example.ghifar.footballmatch.presenter.ApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
 
 /**
  * Created by Alhudaghifari on 20:09 20/10/18
  *
  */
+class ListTeamsPresenter(private val view: ListTeamsInterface) {
+
+    private var disposable: Disposable? = null
+
+    private val footballApiService by lazy {
+        ApiService.create()
+    }
+
+    fun disposeThis() {
+        disposable?.dispose()
+    }
+
+    fun getListTeamByLeague(leagueName: String) {
+        view.showLoading()
+        disposable = footballApiService.getListTeamByLeagueName(leagueName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            view.showTeamList(result.teams)
+                        },
+                        { error ->
+                            view.showError(error.message + " 1")
+                            view.hideLoading()
+                        }
+                )
+    }
+}
