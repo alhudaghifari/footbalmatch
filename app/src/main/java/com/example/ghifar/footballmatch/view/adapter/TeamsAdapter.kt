cd1c1e9bfddd686363987993b1250a93b336f45f
alhudaghifari <alhudaghifari@gmail.com>
2018-10-20 21:31:00 +0700

    modified adapter teams

--- app/src/main/java/com/example/ghifar/footballmatch/view/adapter/TeamsAdapter.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/view/adapter/TeamsAdapter.kt
@@ -1,8 +1,77 @@
 package com.example.ghifar.footballmatch.view.adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.ghifar.footballmatch.R
+import com.example.ghifar.footballmatch.R.id.team_badge
+import com.example.ghifar.footballmatch.R.id.team_name
+import com.example.ghifar.footballmatch.model.datateammodel.Team
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
 
 /**
  * Created by Alhudaghifari on 16:59 20/10/18
  *
  */
-class TeamsAdapter{}
+class TeamsAdapter(var context: Context, private val teams: List<Team>, private val listener: (Team) -> Unit)
+    : RecyclerView.Adapter<TeamViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamViewHolder {
+        return TeamViewHolder(TeamUI().createView(AnkoContext.create(parent.context, parent)))
+    }
+
+    override fun getItemCount(): Int {
+        return teams.size
+    }
+
+    override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
+        holder.bindItem(context, teams[position], listener)
+    }
+}
+
+class TeamViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    private val teamBadge: ImageView = view.find(team_badge)
+    private val teamName: TextView = view.find(team_name)
+
+    fun bindItem(context: Context, teams: Team, listener: (Team) -> Unit) {
+        Glide.with(context)
+                .load(teams.strTeamBadge).into(teamBadge)
+        teamName.text = teams.strTeam
+        itemView.onClick { listener(teams) }
+    }
+}
+
+class TeamUI : AnkoComponent<ViewGroup> {
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui) {
+            linearLayout {
+                lparams(width = matchParent, height = wrapContent)
+                padding = dip(16)
+                orientation = LinearLayout.HORIZONTAL
+
+                imageView {
+                    id = R.id.team_badge
+                }.lparams{
+                    height = dip(50)
+                    width = dip(50)
+                }
+
+                textView {
+                    id = R.id.team_name
+                    textSize = 16f
+                }.lparams{
+                    margin = dip(15)
+                }
+
+            }
+        }
+    }
+
+}
