515c92b71a8397d690df4bf45ad7d8ac81220823
alhudaghifari <alhudaghifari@gmail.com>
2018-10-20 21:30:22 +0700

    team interface and presenter modified

--- app/src/main/java/com/example/ghifar/footballmatch/presenter/teams/TeamInterface.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/presenter/teams/TeamInterface.kt
@@ -1,9 +1,14 @@
 package com.example.ghifar.footballmatch.presenter.teams
 
+import com.example.ghifar.footballmatch.model.datateammodel.Team
 
 /**
  * Created by Alhudaghifari on 19:47 20/10/18
  *
  */
 interface TeamInterface {
+    fun showLoading()
+    fun hideLoading()
+    fun showTeamList(data: List<Team>)
+    fun showError(msg: String)
 }
--- app/src/main/java/com/example/ghifar/footballmatch/presenter/teams/TeamPresenter.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/presenter/teams/TeamPresenter.kt
@@ -1,7 +1,40 @@
 package com.example.ghifar.footballmatch.presenter.teams
 
+import com.example.ghifar.footballmatch.presenter.ApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
 
 /**
  * Created by Alhudaghifari on 19:46 20/10/18
  *
  */
+class TeamPresenter(private val view: TeamInterface) {
+
+    private var disposable: Disposable? = null
+
+    private val footballApiService by lazy {
+        ApiService.create()
+    }
+
+    fun disposeThis() {
+        disposable?.dispose()
+    }
+
+    fun getDetailTeam(teamName: String) {
+        view.showLoading()
+        disposable = footballApiService.getDetailTeamById(teamName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            view.showTeamList(result.teams)
+                        },
+                        { error ->
+                            view.showError(error.message + " 1")
+                            view.hideLoading()
+                        }
+                )
+    }
+}
