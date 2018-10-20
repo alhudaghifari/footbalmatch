6ae5063f80cfd1e2fdd6be8fcb054b4689ad2e8e
alhudaghifari <alhudaghifari@gmail.com>
2018-10-20 17:12:20 +0700

    fragment teams added

--- app/src/main/java/com/example/ghifar/footballmatch/view/adapter/TeamsAdapter.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/view/adapter/TeamsAdapter.kt
@@ -5,3 +5,4 @@ package com.example.ghifar.footballmatch.view.adapter
  * Created by Alhudaghifari on 16:59 20/10/18
  *
  */
+class TeamsAdapter{}
--- app/src/main/java/com/example/ghifar/footballmatch/view/fragment/TeamsFragment.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/view/fragment/TeamsFragment.kt
@@ -3,27 +3,59 @@ package com.example.ghifar.footballmatch.view.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 import com.example.ghifar.footballmatch.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.ghifar.footballmatch.R.array.league
+import org.jetbrains.anko.support.v4.ctx
 
 /**
- * A simple [Fragment] subclass.
- *
+ * Created by alhudaghifari on 20 Oktober 2018
  */
 class TeamsFragment : Fragment() {
 
+    private val TAG = TeamsFragment::class.java.simpleName
+
+    private lateinit var spinnerTeams: Spinner
+    private lateinit var leagueName: String
+    private lateinit var myToolbar: Toolbar
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teams, container, false)
+        val v = inflater.inflate(R.layout.fragment_teams, container, false)
+
+        myToolbar = v.findViewById(R.id.my_toolbar) as Toolbar
+
+        // setup toolbar
+        myToolbar.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+        myToolbar.setTitleTextColor(resources.getColor(R.color.white))
+        myToolbar.setTitle(resources.getString(R.string.title_matches))
+        (activity as AppCompatActivity).setSupportActionBar(myToolbar)
+
+        spinnerTeams = v.findViewById(R.id.spinnerTeams)
+        val spinnerItems = resources.getStringArray(league)
+        val spinnerAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
+        spinnerTeams.adapter = spinnerAdapter
+
+        spinnerTeams.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                leagueName = spinnerTeams.selectedItem.toString()
+                Log.d(TAG, "spinner leagueName : $leagueName")
+//                presenter.getTeamList(leagueName)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        return v
     }
 
 
