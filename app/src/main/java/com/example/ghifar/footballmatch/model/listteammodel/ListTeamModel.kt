0122977ca105cea894a9dd8a310a0a1f58b6276d
alhudaghifari <alhudaghifari@gmail.com>
2018-10-20 21:28:00 +0700

    list team model added

--- app/src/main/java/com/example/ghifar/footballmatch/model/listteammodel/ListTeamModel.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/model/listteammodel/ListTeamModel.kt
@@ -1,7 +1,11 @@
 package com.example.ghifar.footballmatch.model.listteammodel
 
+import com.example.ghifar.footballmatch.model.datateammodel.Team
+
 
 /**
  * Created by Alhudaghifari on 20:12 20/10/18
  *
  */
+class ListTeamModel(
+        val teams: List<Team>)
