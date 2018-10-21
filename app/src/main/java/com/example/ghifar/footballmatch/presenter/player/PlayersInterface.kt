e1aa5bb4d01547a742e78556cb31a42edacedeac
alhudaghifari <alhudaghifari@gmail.com>
2018-10-22 06:14:21 +0700

    interface modified

--- app/src/main/java/com/example/ghifar/footballmatch/presenter/player/PlayersInterface.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/presenter/player/PlayersInterface.kt
@@ -1,9 +1,15 @@
 package com.example.ghifar.footballmatch.presenter.player
 
+import com.example.ghifar.footballmatch.model.playermodel.Player
+
 
 /**
  * Created by Alhudaghifari on 23:34 21/10/18
  *
  */
 interface PlayersInterface {
+    fun showLoading()
+    fun hideLoading()
+    fun showTeamList(data: List<Player>)
+    fun showError(msg: String)
 }
