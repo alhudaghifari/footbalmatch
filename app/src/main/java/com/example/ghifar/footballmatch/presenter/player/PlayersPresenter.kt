6d928b536fe98ceec889af487567a2be44decaf3
alhudaghifari <alhudaghifari@gmail.com>
2018-10-22 06:14:34 +0700

    presenter modified

--- app/src/main/java/com/example/ghifar/footballmatch/presenter/player/PlayersPresenter.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/presenter/player/PlayersPresenter.kt
@@ -1,7 +1,42 @@
 package com.example.ghifar.footballmatch.presenter.player
 
+import com.example.ghifar.footballmatch.presenter.ApiService
+import com.example.ghifar.footballmatch.presenter.teams.ListTeamsInterface
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
 
 /**
  * Created by Alhudaghifari on 23:33 21/10/18
  *
  */
+
+class PlayersPresenter (private val view: PlayersInterface) {
+
+    private var disposable: Disposable? = null
+
+    private val footballApiService by lazy {
+        ApiService.create()
+    }
+
+    fun disposeThis() {
+        disposable?.dispose()
+    }
+
+    fun getListPlayerByTeamName(leagueName: String) {
+        view.showLoading()
+        disposable = footballApiService.getListPlayerByTeamName(leagueName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            view.showTeamList(result.player)
+                        },
+                        { error ->
+                            view.showError(error.message + " 1")
+                            view.hideLoading()
+                        }
+                )
+    }
+}
