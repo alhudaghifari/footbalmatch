3db5a34f0962cf9f2b4ce06ae99e425be86528ee
alhudaghifari <alhudaghifari@gmail.com>
2018-10-22 07:55:41 +0700

    data modified

--- app/src/main/java/com/example/ghifar/footballmatch/model/playermodel/OnePlayerModel.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/model/playermodel/OnePlayerModel.kt
@@ -1,7 +1,16 @@
 package com.example.ghifar.footballmatch.model.playermodel
 
+import com.google.gson.annotations.SerializedName
+
 
 /**
  * Created by Alhudaghifari on 7:54 22/10/18
  *
  */
+
+class OnePlayerModel {
+
+    @SerializedName("players")
+    var player: List<Player> = mutableListOf()
+
+}
