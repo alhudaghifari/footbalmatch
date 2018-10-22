ef4acacf0f3229a38f4ffd0813a7524b2324d8c9
alhudaghifari <alhudaghifari@gmail.com>
2018-10-22 11:08:03 +0700

    add API in presenter

--- app/src/main/java/com/example/ghifar/footballmatch/presenter/player/DetailPlayerPresenter.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/presenter/player/DetailPlayerPresenter.kt
@@ -1,7 +1,41 @@
 package com.example.ghifar.footballmatch.presenter.player
 
+import com.example.ghifar.footballmatch.presenter.ApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
 
 /**
  * Created by Alhudaghifari on 7:57 22/10/18
  *
  */
+class DetailPlayerPresenter (private val view: PlayersInterface) {
+
+    private var disposable: Disposable? = null
+
+    private val footballApiService by lazy {
+        ApiService.create()
+    }
+
+    fun disposeThis() {
+        disposable?.dispose()
+    }
+
+    fun getDetailPlayerById(id: String) {
+        view.showLoading()
+        disposable = footballApiService.getDetailPlayerById(id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            view.showTeamList(result.player)
+                            view.hideLoading()
+                        },
+                        { error ->
+                            view.showError(error.message + " 1")
+                            view.hideLoading()
+                        }
+                )
+    }
+}
