c425d5c6ce27c2ab429f1b222bfbc47a1eb2f654
alhudaghifari <alhudaghifari@gmail.com>
2018-10-22 07:55:21 +0700

    player file added

---
+++ app/src/main/java/com/example/ghifar/footballmatch/model/playermodel/OnePlayerModel.kt
@@ -0,0 +1,7 @@
+package com.example.ghifar.footballmatch.model.playermodel
+
+
+/**
+ * Created by Alhudaghifari on 7:54 22/10/18
+ *
+ */
---
+++ app/src/main/java/com/example/ghifar/footballmatch/view/activity/PlayerDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ghifar.footballmatch.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_detail)
+    }
+}
