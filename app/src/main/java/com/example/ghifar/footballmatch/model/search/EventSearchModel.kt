d4eebaabd32a8faa3ff85c3b3cf08a34d727dd78
alhudaghifari <alhudaghifari@gmail.com>
2018-10-23 22:42:33 +0700

    model for event search added

--- app/src/main/java/com/example/ghifar/footballmatch/model/search/EventSearchModel.kt
+++ app/src/main/java/com/example/ghifar/footballmatch/model/search/EventSearchModel.kt
@@ -1,7 +1,17 @@
 package com.example.ghifar.footballmatch.model.search
 
+import com.example.ghifar.footballmatch.model.eventleaguemodel.Event
+import com.google.gson.annotations.SerializedName
+
 
 /**
  * Created by Alhudaghifari on 19:54 23/10/18
  *
  */
+
+class EventSearchModel {
+
+    @SerializedName("event")
+    var events: List<Event> = mutableListOf()
+
+}
