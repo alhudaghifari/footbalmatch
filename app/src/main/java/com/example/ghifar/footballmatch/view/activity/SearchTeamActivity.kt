edeb325296e87ed32acfaccdb2c5f50b8ca923e0
alhudaghifari <alhudaghifari@gmail.com>
2018-10-23 23:31:35 +0700

    add new file

---
+++ app/src/main/java/com/example/ghifar/footballmatch/view/activity/SearchTeamActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ghifar.footballmatch.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchTeamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_team)
+    }
+}
